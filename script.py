110bbc331fc0870d97314c5fe1f14e9608bd1b0f
kbk4lego <32390320+kbk4lego@users.noreply.github.com>
2023-09-01 21:42:58 -0400

    base.html updates
    created container for base.html

--- script.py
+++ script.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
+@app.route('/nav')
+def nav():
+	return render_template('nav.html')
 
+@app.route('/<string:url>')
+def sign_in(url):
+	return render_template(f'{url}.html')
 
 if __name__ == '__main__':
 		app.run(debug=True)
